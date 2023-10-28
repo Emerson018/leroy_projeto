145b32a5b1cfee6aaf865d31fc7d1b1f4c52ef75
Emerson Viçosa <emersonvicosa@gmail.com>
2023-10-27 22:26:48 -0300

    add novo template

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from produtos.views import index
+from produtos.views import index, imagem
 
 urlpatterns = [
-    path('', index),
-
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
