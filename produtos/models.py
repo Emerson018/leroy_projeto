27756ebd82a70546ed9132eef1c8e0c76e430dfd
Emerson Viçosa <emersonvicosa@gmail.com>
2023-10-28 22:54:36 -0300

    criando um banco de dados com os lms

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    lm = models.CharField(max_length=20, null=False, blank=False)
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    preco = models.FloatField(max_length=10, null=False, blank=False)
+
+    def __str__(self):
+        return f"LM [lm={self.lm}]"
--- produtos/views.py
+++ produtos/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'produtos/index.html')
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+        "legenda": "webbtelescope.org/NASA"},
+        2: {"nome": "Galáxia NGC",
+        "legenda": "nasa.org/NASA"}
+    }
+
+    return render(request, 'produtos/index.html',{"cards": dados})
 
 def imagem(request):
     return render(request, 'produtos/imagem.html')
