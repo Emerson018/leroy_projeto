3969b1ac8d0cc5a96af1ac9062a106cf37fbb9a3
Emerson Viçosa <emersonvicosa@gmail.com>
2023-11-18 22:05:47 -0300

    add lógica para cadastro, login e logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,17 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
-
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 def login(request):
     form = LoginForms()
 
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        if usuario is not None:
+            auth.login(request,usuario)
+            messages.success(request,f"{nome} logado com sucesso!")
+
+            return redirect('lista')
+
+        else:
+            messages.error(request,"Erro ao efetuar login.")
+
+            return redirect('login')
+
+
     return render(request, 'usuarios/login.html', {"form": form})
     
 def logout(request):
-
-
-    return render(request, 'usuarios/logout.html')
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso!")
+    
+    return redirect('login')
 
 def register(request):
-    form = CadastroForms
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        
+        if form.is_valid():
+            if form["senha1"].value() != form["senha2"].value():
+                messages.error(request, "Senha não são iguais.")
+
+                return redirect('register')
+            
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha1"].value()
+
+            if User.objects.filter(username=nome).exists():
+                messages.error(request,'Usuário já existe.')
+
+                return redirect('register')
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            usuario.save()
+            messages.success(request,"Cadastro realizado com sucesso!")
+            return redirect('login')
+        
     return render(request, 'usuarios/register.html', {"form": form})
